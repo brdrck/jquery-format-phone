626f9d09d2a9ffa0759d805ac2adc4c1e2db318d
Jeff Broderick <jeff@brdrck.me>
2012-01-13 22:47:25 -0800

    Fixed backspace, more reliable, minor bugs
    Backspace now completely works.
    You can enter more than the minimum and it treats it as a normal number.
    You can select and delete the whole input.
    Cleaned up some code.

--- src/format_phone_input.js
+++ src/format_phone_input.js
@@ -3,42 +3,29 @@
   function format(e) {
     var input = $(this)
     var digits = input.val().replace(/[^\d]/g, '');
-    
-    if (e.which == 8 && digits.length <= 3) {
-      return;
+        
+    if (e.which == 17 || e.which == 18 || e.which == 91) {
+          return;
     }
     
-    if (e.ctrlKey || e.shiftKey || e.metaKey) {
-      return;
-    }
-
-    if (e.which == 8 && digits.length > 3) {
-      input.val(digits.substring(0, digits.length - 1));
-      input.trigger('keydown');
-    }
-
-    if (digits.length < 3) {
-      return;
-    }
-
-    if ((digits.length >= 3 && digits.length <= 6)) {
+    
+    if ((digits.length > 3 && digits.length <= 6)) {
       var prefix = digits.substring(0, 3);
       var rest = digits.substring(3, digits.length);
       return input.val(prefix + '-' + rest);
-    }
-
-    if (digits.length > 6 && digits.length <= 10) {
+    } else if (digits.length > 6 && digits.length <= 10) {
       var areaCode = digits.substring(0, 3);
       var prefix = digits.substring(3, 6);
       var rest = digits.substring(6, digits.length)
       return input.val('(' + areaCode + ') ' + prefix + '-' + rest);
+    } else {
+    	return input.val(digits);
     }
 
     return;
   }
 
   $.fn.formatsPhone = function() {
-    this.bind('keydown', format);
-    this.attr('maxlength', '14');
+    this.bind('keyup', format);
   };
 })(jQuery);
